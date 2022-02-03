3bccd6c1a201d1352bfd272d3d65d601c9c6f458
SaviHerath <savi@chaebanicecream.com>
2022-02-03 23:29:17 +0530

    Video play function

--- script.js
+++ script.js
@@ -11,9 +11,21 @@ const currentTime = document.querySelector('.time-elapsed');
 const duration = document.querySelector('.time-duration');
 const fullscreenBtn = document.querySelector('.fullscreen');
 
-// Play & Pause ----------------------------------- //
-
-
+function showPlayIcon() {
+    playBtn.classList.replace('fa-pause', 'fa-play');
+    playBtn.setAttribute('title', 'Play');
+  }
+  
+  function togglePlay() {
+    if (video.paused) {
+      video.play();
+      playBtn.classList.replace('fa-play', 'fa-pause');
+      playBtn.setAttribute('title', 'Pause');
+    } else {
+      video.pause();
+      showPlayIcon();
+    }
+  }
 
 // Progress Bar ---------------------------------- //
 
@@ -29,4 +41,7 @@ const fullscreenBtn = document.querySelector('.fullscreen');
 
 // Fullscreen ------------------------------- //
 
-
+playBtn.addEventListener('click', togglePlay);
+video.addEventListener('click', togglePlay);
+video.addEventListener('timeupdate', updateProgress);
+video.addEventListener('canplay', updateProgress);
